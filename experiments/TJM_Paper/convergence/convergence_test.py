a95939945bc53c0e497f57e23c21d4b680826e90
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2025-05-07 19:09:46 +0200

    combined tests into one file

--- experiments/TJM_Paper/convergence/convergence_test.py
+++ experiments/TJM_Paper/convergence/convergence_test.py
@@ -6,113 +6,138 @@ import qutip as qt
 
 from mqt.yaqs.core.data_structures.networks import MPO, MPS
 from mqt.yaqs.core.data_structures.noise_model import NoiseModel
+from mqt.yaqs.core.libraries.gate_library import X
 from mqt.yaqs.core.data_structures.simulation_parameters import Observable, PhysicsSimParams
 from mqt.yaqs import simulator
 
-# Define the system Hamiltonian
-L = 10
-J = 1
-g = 0.5
-H_0 = MPO()
-H_0.init_ising(L, J, g)
 
-# Define the initial state
-state = MPS(L, state='zeros')
-
-# Define the noise model
-gamma_relaxation = 0.1
-gamma_dephasing = 0.1
-noise_model = NoiseModel(['relaxation', 'dephasing'], [gamma_relaxation, gamma_dephasing])
-
-
-
-fig, ax = plt.subplots(2, 1)
 if __name__ == "__main__":
+    # Define the system Hamiltonian
+    L = 10
+    J = 1
+    g = 0.5
+    H_0 = MPO()
+    H_0.init_ising(L, J, g)
+
+    # Define the initial state
+    state = MPS(L, state='zeros')
+
+    # Define the noise model
+    gamma_relaxation = 0.1
+    gamma_dephasing = 0.1
+    noise_model = NoiseModel(['relaxation', 'dephasing'], [gamma_relaxation, gamma_dephasing])
+
     # Define the simulation parameters
     T = 1
-    dt = 0.5
     sample_timesteps = False
     N = 10001
     threshold = 0
     max_bond_dim = 2**L
+
+    dt = 0.1
     order = 2
-    measurements = [Observable('x', site) for site in range(L)]
+    measurements = [Observable(X(), site) for site in range(L)]
     sim_params = PhysicsSimParams(measurements, T, dt, N, max_bond_dim, threshold, order, sample_timesteps=sample_timesteps)
     simulator.run(state, H_0, sim_params, noise_model)
-
-    filename = f"TJM_Convergence_dt01.pickle"
+    filename = f"TJM_Convergence_order2_dt01.pickle"
     with open(filename, 'wb') as f:
         pickle.dump({
             'sim_params': sim_params,
         }, f)
-    # Expected: 0.3327520802691848
-    # print(sim_params.observables[4].results)
-    # heatmap = []
-    # for observable in sim_params.observables:
-    #     heatmap.append(observable.results)
 
-    # im = ax[0].imshow(heatmap, aspect='auto', extent=[0, T, L, 0])
-    # ax[0].set_ylabel('Site')
+    dt = 0.2
+    order = 2
+    measurements = [Observable(X(), site) for site in range(L)]
+    sim_params = PhysicsSimParams(measurements, T, dt, N, max_bond_dim, threshold, order, sample_timesteps=sample_timesteps)
+    simulator.run(state, H_0, sim_params, noise_model)
+    filename = f"TJM_Convergence_order2_dt02.pickle"
+    with open(filename, 'wb') as f:
+        pickle.dump({
+            'sim_params': sim_params,
+        }, f)
 
-    # # Centers site ticks
-    # ax[0].set_yticks([x-0.5 for x in list(range(1,L+1))], range(1,L+1))
-    # cbar = plt.colorbar(im, ax=ax[0])
-    # cbar.ax.set_title('$\\langle X \\rangle$')
-    # ########################################
+    dt = 0.5
+    order = 2
+    measurements = [Observable(X(), site) for site in range(L)]
+    sim_params = PhysicsSimParams(measurements, T, dt, N, max_bond_dim, threshold, order, sample_timesteps=sample_timesteps)
+    simulator.run(state, H_0, sim_params, noise_model)
+    filename = f"TJM_Convergence_order2_dt05.pickle"
+    with open(filename, 'wb') as f:
+        pickle.dump({
+            'sim_params': sim_params,
+        }, f)
 
-    # ####### QuTip Exact Solver ############
-    # # Time vector
-    # t = np.arange(0, sim_params.elapsed_time + sim_params.dt, 0.1)
+    dt = 0.1
+    order = 1
+    measurements = [Observable(X(), site) for site in range(L)]
+    sim_params = PhysicsSimParams(measurements, T, dt, N, max_bond_dim, threshold, order, sample_timesteps=sample_timesteps)
+    simulator.run(state, H_0, sim_params, noise_model)
+    filename = f"TJM_Convergence_order1_dt01.pickle"
+    with open(filename, 'wb') as f:
+        pickle.dump({
+            'sim_params': sim_params,
+        }, f)
 
-    # # Define Pauli matrices
-    # sx = qt.sigmax()
-    # sy = qt.sigmay()
-    # sz = qt.sigmaz()
+    dt = 0.2
+    order = 1
+    measurements = [Observable(X(), site) for site in range(L)]
+    sim_params = PhysicsSimParams(measurements, T, dt, N, max_bond_dim, threshold, order, sample_timesteps=sample_timesteps)
+    simulator.run(state, H_0, sim_params, noise_model)
+    filename = f"TJM_Convergence_order1_dt02.pickle"
+    with open(filename, 'wb') as f:
+        pickle.dump({
+            'sim_params': sim_params,
+        }, f)
 
-    # # Construct the Ising Hamiltonian
-    # H = 0
-    # for i in range(L-1):
-    #     H += J * qt.tensor([sz if n==i or n==i+1 else qt.qeye(2) for n in range(L)])
-    # for i in range(L):
-    #     H += g * qt.tensor([sx if n==i else qt.qeye(2) for n in range(L)])
+    dt = 0.5
+    order = 1
+    measurements = [Observable(X(), site) for site in range(L)]
+    sim_params = PhysicsSimParams(measurements, T, dt, N, max_bond_dim, threshold, order, sample_timesteps=sample_timesteps)
+    simulator.run(state, H_0, sim_params, noise_model)
+    filename = f"TJM_Convergence_order1_dt05.pickle"
+    with open(filename, 'wb') as f:
+        pickle.dump({
+            'sim_params': sim_params,
+        }, f)
 
-    # # Construct collapse operators
-    # c_ops = []
+    ####### QuTip Exact Solver ############
+    # Time vector
+    t = np.arange(0, sim_params.elapsed_time + sim_params.dt, 0.1)
 
-    # # Dephasing operators
-    # for i in range(L):
-    #     c_ops.append(np.sqrt(gamma_dephasing) * qt.tensor([sz if n==i else qt.qeye(2) for n in range(L)]))
+    # Define Pauli matrices
+    sx = qt.sigmax()
+    sy = qt.sigmay()
+    sz = qt.sigmaz()
 
-    # # Relaxation operators
-    # for i in range(L):
-    #     c_ops.append(np.sqrt(gamma_relaxation) * qt.tensor([qt.destroy(2) if n==i else qt.qeye(2) for n in range(L)]))
+    # Construct the Ising Hamiltonian
+    H = 0
+    for i in range(L-1):
+        H += J * qt.tensor([sz if n==i or n==i+1 else qt.qeye(2) for n in range(L)])
+    for i in range(L):
+        H += g * qt.tensor([sx if n==i else qt.qeye(2) for n in range(L)])
 
-    # # Initial state
-    # psi0 = qt.tensor([qt.basis(2, 0) for _ in range(L)])
+    # Construct collapse operators
+    c_ops = []
 
-    # # Define measurement operators
-    # sx_list = [qt.tensor([sx if n == i else qt.qeye(2) for n in range(L)]) for i in range(L)]
+    # Dephasing operators
+    for i in range(L):
+        c_ops.append(np.sqrt(gamma_dephasing) * qt.tensor([sz if n==i else qt.qeye(2) for n in range(L)]))
 
-    # # Exact Lindblad solution
-    # result_lindblad = qt.mesolve(H, psi0, t, c_ops, sx_list, progress_bar=True)
-    # filename = f"QuTip_exact_convergence.pickle"
-    # with open(filename, 'wb') as f:
-    #     pickle.dump({
-    #         'sim_params': sim_params,
-    #         'observables': result_lindblad.expect,
-    #     }, f)
-    # heatmap2 = []
-    # for site in range(len(sx_list)):
-    #         heatmap2.append(result_lindblad.expect[site])
+    # Relaxation operators
+    for i in range(L):
+        c_ops.append(np.sqrt(gamma_relaxation) * qt.tensor([qt.destroy(2) if n==i else qt.qeye(2) for n in range(L)]))
 
-    # # Error heatmap
-    # heatmap = np.array(heatmap)
-    # heatmap2 = np.array(heatmap2)
-    # im2 = ax[1].imshow(np.abs(heatmap2-heatmap), cmap='Reds', aspect='auto', extent=[0, T, L, 0], norm=LogNorm())
-    # ax[1].set_yticks([x-0.5 for x in list(range(1,L+1))], range(1,L+1))
+    # Initial state
+    psi0 = qt.tensor([qt.basis(2, 0) for _ in range(L)])
 
-    # cbar = plt.colorbar(im2, ax=ax[1])
-    # cbar.ax.set_title('Error')
-    # ax[1].set_xlabel('t')
-    # ax[1].set_ylabel('Site')
-    # plt.show()
+    # Define measurement operators
+    sx_list = [qt.tensor([sx if n == i else qt.qeye(2) for n in range(L)]) for i in range(L)]
+
+    # Exact Lindblad solution
+    result_lindblad = qt.mesolve(H, psi0, t, c_ops, sx_list, progress_bar=True)
+    filename = f"QuTip_exact_convergence.pickle"
+    with open(filename, 'wb') as f:
+        pickle.dump({
+            'sim_params': sim_params,
+            'observables': result_lindblad.expect,
+        }, f)
