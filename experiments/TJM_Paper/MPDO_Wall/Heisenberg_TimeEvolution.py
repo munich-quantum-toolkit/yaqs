65d0504658718501a35ee6801745fdfd7e68b0a5
MaxFroehlich1410 <froehlich@wias-berlin.de>
2025-05-08 15:59:23 +0200

    added 30L jupyter notebook

--- experiments/TJM_Paper/MPDO_Wall/Heisenberg_TimeEvolution.py
+++ experiments/TJM_Paper/MPDO_Wall/Heisenberg_TimeEvolution.py
@@ -1,8 +1,4 @@
-import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
-import numpy as np
 import pickle
-import time
 
 from mqt.yaqs.core.data_structures.networks import MPO, MPS
 from mqt.yaqs.core.data_structures.noise_model import NoiseModel
@@ -10,58 +6,39 @@ from mqt.yaqs.core.data_structures.simulation_parameters import Observable, Phys
 from mqt.yaqs import simulator
 from mqt.yaqs.core.libraries.gate_library import Z
 
-
-# Define the system Hamiltonian
-L = 30
-d = 2
-J = 1
-g = 0.5
-H_0 = MPO()
-# H_0.init_Ising(L, d, J, g)
-H_0.init_heisenberg(L, J, J, J, g)
-
-# Define the initial state
-state = MPS(L, state='wall')
-
-# Define the noise model
-gamma_relaxation = 0.1
-gamma_dephasing = 0.1
-noise_model = NoiseModel(['relaxation', 'excitation'], [gamma_relaxation, gamma_dephasing])
-
-# Define the simulation parameters
-T = 10
-dt = 0.1
-sample_timesteps = True
-N = 100
-max_bond_dim = 8
-threshold = 0
-order = 2
-measurements = [Observable(Z(), site) for site in range(L)]
-sim_params = PhysicsSimParams(measurements, T, dt, N, max_bond_dim, threshold, order, sample_timesteps = True)
-
-fig, ax = plt.subplots(1, 1)
 if __name__ == "__main__":
+        # Define the system Hamiltonian
+    L = 30
+    J = 1
+    g = 0.5
+    H_0 = MPO()
+    # H_0.init_Ising(L, d, J, g)
+    H_0.init_heisenberg(L, J, J, J, g)
+
+    # Define the initial state
+    state = MPS(L, state='wall')
+
+    # Define the noise model
+    gamma_relaxation = 0.1
+    gamma_dephasing = 0.1
+    noise_model = NoiseModel(['relaxation', 'excitation'], [gamma_relaxation, gamma_dephasing])
+
+    # Define the simulation parameters
+    T = 10
+    dt = 0.1
+    sample_timesteps = True
+    N = 100
+    max_bond_dim = 8
+    threshold = 0
+    order = 2
+    measurements = [Observable(Z(), site) for site in range(L)]
+    sim_params = PhysicsSimParams(measurements, T, dt, N, max_bond_dim, threshold, order, sample_timesteps = True)
+
     ########## TJM Example #################
-    start_time = time.time()
     simulator.run(state, H_0, sim_params, noise_model)
-    print(time.time() - start_time)
 
-    heatmap = []
-    for observable in sim_params.observables:
-        heatmap.append(observable.results)
-
-    im = ax.imshow(heatmap, aspect='auto', extent=[0, T, L, 0])
-    ax.set_ylabel('Site')
-
-    # Centers site ticks
-    ax.set_yticks([x-0.5 for x in list(range(1,L+1))], range(1,L+1))
-    cbar = plt.colorbar(im, ax=ax)
-    cbar.ax.set_title('$\\langle Z \\rangle$')
-    #########################################
     filename = f"30L_NoNoise.pickle"
     with open(filename, 'wb') as f:
         pickle.dump({
             'sim_params': sim_params,
         }, f)
-
-    plt.show()
