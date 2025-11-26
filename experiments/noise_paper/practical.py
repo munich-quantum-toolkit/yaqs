849d33b36d34889b2a2db2e36517806a1b5a5689
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2025-11-26 11:15:29 +0100

    updated practical test

--- experiments/noise_paper/practical.py
+++ experiments/noise_paper/practical.py
@@ -36,14 +36,11 @@ if __name__ == "__main__":
     H_0 = MPO()
     H_0.init_ising(L, J, h)
 
-    # 1000, 500, 400, 250, 200, 125, 100, 50, 25, 20 steps
-    dt_list = [0.01, 0.0125, 0.02, 0.025, 0.04, 0.05, 0.1, 0.2, 0.25]
+    # 1000, 500, 400, 250, 200, 125, 100, 50, 25, 20, 10 steps
+    dt_list = [0.01, 0.0125, 0.02, 0.025, 0.04, 0.05, 0.1, 0.2, 0.25, 0.5]
 
     for k, dt in enumerate(dt_list):
-        # gamma_list = [0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50]\
-        # gamma_list = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10]
-        # gamma_list = [0.01, 0.02, 0.05]
-        gamma_list = [0.02, 0.05]
+        gamma_list = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50]
 
         results1 = []
         results2 = []
@@ -67,7 +64,7 @@ if __name__ == "__main__":
             # cost = tdvp_simulator(H_0, noise_model)
             # results2.append(cost)
 
-        filename = f"u1_low_gamma_practical_{k+1}.pickle"
+        filename = f"u1_practical_{k}.pickle"
         with open(filename, 'wb') as handle:
             pickle.dump(results1, handle)
 
