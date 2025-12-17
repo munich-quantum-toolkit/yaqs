7d7d0bc7d7f8836ddbb70d93362ab7e0fc17df01
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2025-12-17 22:06:15 +0100

    upload wall time data

--- experiments/noise_paper/wall_time.py
+++ experiments/noise_paper/wall_time.py
@@ -41,9 +41,7 @@ if __name__ == "__main__":
     H_0.init_ising(L, J, h)
 
     # 1000, 500, 400, 250, 200, 125, 100, 50, 25, 20, 10 steps
-    # dt_list = [0.001, 0.0125, 0.0025, 0.004, 0.005, 0.01, 0.0125, 0.02, 0.025, 0.04, 0.05, 0.1, 0.2, 0.25, 0.5]
-    # dt_list = [0.0025, 0.004, 0.005, 0.01, 0.0125, 0.02, 0.025, 0.04, 0.05, 0.1, 0.2, 0.25, 0.5]
-    dt_list = [0.001, 0.00125, 0.0025, 0.004, 0.005, 0.01, 0.0125, 0.02, 0.025, 0.04, 0.05, 0.1, 0.2, 0.25, 0.5]
+    dt_list = [0.0025, 0.004, 0.005, 0.01, 0.0125, 0.02, 0.025, 0.04, 0.05, 0.1, 0.2, 0.25, 0.5]
 
     for k, dt in enumerate(dt_list):
         gamma_list = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50]
@@ -70,7 +68,7 @@ if __name__ == "__main__":
             # cost = tdvp_simulator(H_0, noise_model)
             # results2.append(cost)
 
-        filename = f"walltime_{k}.pickle"
+        filename = f"walltime_{k+4}.pickle"
         with open(filename, 'wb') as handle:
             pickle.dump(results1, handle)
 
