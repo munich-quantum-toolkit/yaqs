0e75c0fcded5eaefe7cf26fd55d548f903f9abc7
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2025-12-17 04:03:51 +0100

    data upload

--- experiments/noise_paper/practical_2.py
+++ experiments/noise_paper/practical_2.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     # 1000, 500, 400, 250, 200, 125, 100, 50, 25, 20, 10 steps
     # dt_list = [0.001, 0.0125, 0.0025, 0.004, 0.005, 0.01, 0.0125, 0.02, 0.025, 0.04, 0.05, 0.1, 0.2, 0.25, 0.5]
     # dt_list = [0.0025, 0.004, 0.005, 0.01, 0.0125, 0.02, 0.025, 0.04, 0.05, 0.1, 0.2, 0.25, 0.5]
-    dt_list = [0.01, 0.0125, 0.02, 0.025, 0.04, 0.05, 0.1, 0.2, 0.25, 0.5]
+    dt_list = [0.0025, 0.004, 0.005, 0.01, 0.0125, 0.02, 0.025, 0.04, 0.05, 0.1, 0.2, 0.25, 0.5]
 
     for k, dt in enumerate(dt_list):
         gamma_list = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50]
@@ -66,7 +66,7 @@ if __name__ == "__main__":
             # cost = tdvp_simulator(H_0, noise_model)
             # results2.append(cost)
 
-        filename = f"cost_{k}.pickle"
+        filename = f"cost_{k+1}.pickle"
         with open(filename, 'wb') as handle:
             pickle.dump(results1, handle)
 
