28a48e6208470dff8fafd9a2b9345acdbe895798
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2026-02-23 15:35:51 +0800

    added missing init

---
+++ tests/characterization/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2025 Chair for Design Automation, TUM
+# All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+# Licensed under the MIT License
+"""Integration tests for characterization module."""  
