dddd2810ee092d673bc6a66fcec17c322f20fc66
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2026-02-26 11:52:29 +0800

    removed old implementations

--- src/mqt/yaqs/characterization/tomography/__init__.py
+++ src/mqt/yaqs/characterization/tomography/__init__.py
@@ -8,8 +8,6 @@
 """Tomography module for YAQS."""
 
 from .process_tensor import ProcessTensor
-from .restricted_process_tensor import RestrictedProcessTensor
-from .restricted_tomography import run as run_restricted
 from .tomography import run
 
-__all__ = ["ProcessTensor", "RestrictedProcessTensor", "run", "run_restricted"]
+__all__ = ["ProcessTensor", "run"]
