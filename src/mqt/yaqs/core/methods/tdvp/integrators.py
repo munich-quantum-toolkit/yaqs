6c8d0032f32a3149759aa78c6824a801d2633bf8
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2026-09-01 18:17:47 +0200

    added center tracking for 1 site TDVP

--- src/mqt/yaqs/core/methods/tdvp/integrators.py
+++ src/mqt/yaqs/core/methods/tdvp/integrators.py
@@ -110,6 +110,7 @@ def sweep_1site(
             krylov_tol=sim_params.krylov_tol,
         )
         state.tensors[i + 1] = oe.contract(state.tensors[i + 1], (0, 3, 2), bond_tensor, (1, 3), (0, 1, 2))
+        state.set_center(i + 1)
 
     last = num_sites - 1
     state.tensors[last] = update_site(
@@ -125,9 +126,9 @@ def sweep_1site(
 
     # Right-to-left sweep: Update sites 1 to L-1.
     for i in reversed(range(1, num_sites)):
-        state.tensors[i] = state.tensors[i].transpose((0, 2, 1))
-        tensor_shape = state.tensors[i].shape
-        reshaped_tensor = state.tensors[i].reshape((tensor_shape[0] * tensor_shape[1], tensor_shape[2]))
+        transposed_tensor = state.tensors[i].transpose((0, 2, 1))
+        tensor_shape = transposed_tensor.shape
+        reshaped_tensor = transposed_tensor.reshape((tensor_shape[0] * tensor_shape[1], tensor_shape[2]))
         site_tensor, bond_tensor = np.linalg.qr(reshaped_tensor)
         state.tensors[i] = site_tensor.reshape((tensor_shape[0], tensor_shape[1], site_tensor.shape[1])).transpose((
             0,
@@ -146,6 +147,7 @@ def sweep_1site(
             krylov_tol=sim_params.krylov_tol,
         )
         state.tensors[i - 1] = oe.contract(state.tensors[i - 1], (0, 1, 3), bond_tensor, (3, 2), (0, 1, 2))
+        state.set_center(i - 1)
         state.tensors[i - 1] = update_site(
             left_blocks[i - 1],
             right_blocks[i - 1],
